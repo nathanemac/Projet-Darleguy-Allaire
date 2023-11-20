d11ceb994f0069f419ab0f5348edc969eaa626d9
nathanemac <91251698+nathanemac@users.noreply.github.com>
2023-11-20 14:41:44 -0500

    début phase 4

--- Phase 1/edge.jl
+++ Phase 1/edge.jl
@@ -66,7 +66,7 @@ function read_edges(header::Dict{String, String}, filename::String)
                   for j = start : start + n_on_this_line - 1
                     # Les lignes suivantes ont été modifiées pour tenir compte du poids des arêtes
                     n_edges = n_edges + 1
-                    edge_weight_value = parse(Float64, data[start+j+1])
+                    edge_weight_value = parse(Float64, data[j+1])
                     if edge_weight_format in ["UPPER_ROW", "LOWER_COL"]
                       edge = (k+1, i+k+2, edge_weight_value)
                     elseif edge_weight_format in ["UPPER_DIAG_ROW", "LOWER_DIAG_COL"]
--- Phase 3/main_p3.jl
+++ Phase 3/main_p3.jl
@@ -5,15 +5,7 @@ include("../Phase 1/read_stsp.jl")
 include("../Phase 1/main.jl")
 include("../Phase 2/utils.jl")
 include("../Phase 2/PriorityQueue.jl")
-
-using LinearAlgebra
-using Printf
-
-##################################
-##### Implémentation de HK : #####
-##################################
-
-#### Minimum 1-tree ####
+include("utils.jl")
 
 function create_graph()
   # 3 : exemple du cours
@@ -42,153 +34,34 @@ function create_graph()
 end
 kruskal, prim, cours = create_graph()
 
-function remove_node_and_edges(graph::ExtendedGraph, special_node::Node) :: ExtendedGraph
-  # Copie le graphe
-  new_graph = deepcopy(graph)
+r = HK(cours) # converge en 15 itérations pour le noeud 1
+poids_tour_cours = sum(map(edge -> edge.weight, r.edges))
 
-  # Supprime les arêtes incidentes au nœud spécial
-  new_graph.edges = filter(e -> e.start_node != special_node && e.end_node != special_node, new_graph.edges)
 
-  # Supprime également le nœud spécial
-  new_graph.nodes = filter(n -> n != special_node, new_graph.nodes)
+graph_bays29 = build_graph("Phase 1/instances/stsp/bays29.tsp", "Graph_Test")
+r2 = HK(graph_bays29, verbose=1000, maxIter=20000) # ne converge pas
+poids_tour_bays29 = sum(map(edge -> edge.weight, r2.edges))
 
-  return new_graph
-end
+graph_swiss42 = build_graph("Phase 1/instances/stsp/swiss42.tsp", "Graph_Test")
+r3 = HK(graph_swiss42, verbose=1000, maxIter=20000) # ne converge pas
+poids_tour_swiss42 = sum(map(edge -> edge.weight, r3.edges))
 
-"Trouve un 1-arbre de recouvrement minimal en partant de special_node"
-function find_minimum_1tree(graph::ExtendedGraph; special_node::Node=graph.nodes[1], kruskal_or_prim = Kruskal)
-  # Retire le nœud spécial et toutes ses arêtes incidentes du graphe
-  subgraph = remove_node_and_edges(graph, special_node)
+graph_gr17 = build_graph("Phase 1/instances/stsp/gr17.tsp", "Graph_Test")
+r4 = HK(graph_gr17, verbose=1000, maxIter=20000) # ne converge pas
+poids_tour_gr17 = sum(map(edge -> edge.weight, r4.edges))
 
-  # Trouve un mst pour subgraph
-  if kruskal_or_prim == Prim
-    mst = Prim(subgraph, st_node = subgraph.nodes[1])
-  else
-    mst = Kruskal(subgraph)
-  end
-  
-  # Récupère les arêtes incidentes du nœud spécial dans le graphe d'origine
-  special_node_edges = filter(e -> e.start_node == special_node || e.end_node == special_node, graph.edges)
 
-  # Trie ces arêtes par poids
-  sorted_edges = sort(special_node_edges, by = e -> e.weight)
 
-  # Sélectionne les deux arêtes les moins chères
-  cheapest_edges = sorted_edges[1:2]
 
-  # Ajoute ces arêtes à l'arbre couvrant minimum pour former un 1-arbre
-  for edge in cheapest_edges
-      push!(mst.edges, edge)
-  end
 
-  # Ajoute special_node à subgraph
-  push!(subgraph.nodes, special_node)
-  sort!(mst.nodes, by = node -> node.name)
-  mst
+
+
+#### HK 2-opt ####
+function two_opt(graph::ExtendedGraph, tour::ExtendedGraph)
+  tour_edges = tour.edges
+  graph_edges = graph.edges
 end
 
 
-#### HK ####
-
-"""Renvoie un vecteur contenant les arêtes reliant le noeud node à ses voisins"""
-function neighbours(graph::ExtendedGraph, node::Node)
-  arêtes_voisins = []
-  for e in graph.edges
-    noeud1 = e.start_node
-    noeud2 = e.end_node 
-    if (node == noeud1 || node == noeud2) 
-      push!(arêtes_voisins, e)
-    end
-  end
-  return arêtes_voisins
-end
-
-function HK(graph::ExtendedGraph; kruskal_or_prim = Kruskal,
-                                  special_node::Node = graph.nodes[1], 
-                                  maxIter::Int = 3000, 
-                                  ϵ::Real = 1e-5, 
-                                  verbose::Int=-1
-                                  )
-
-  graph_copy = deepcopy(graph)
-  ### Initialisation ###
-  n = length(graph_copy.nodes)
-  k = 0
-  πk = zeros(n)
-  Tk = find_minimum_1tree(graph_copy, special_node = special_node, kruskal_or_prim = kruskal_or_prim)
-  tk = 1
-
-  # calcul de dk : 
-  dk = []
-  V = []
-  for node in Tk.nodes
-    voisins = neighbours(Tk, node)
-    push!(V, voisins)
-    push!(dk, length(voisins))
-  end
-
-  # calcul de vk :
-  vk = dk .- 2
-  nvk = norm(vk)
-
-  if verbose > 0 && mod(k, verbose) == 0
-    @info @sprintf "%5s  %9s  %7s " "iter" "tk" "‖vk‖"
-    infoline = @sprintf "%5d  %9.2e  %7.1e" k tk nvk
-  end
-
-  while nvk > ϵ && k < maxIter # vk tend vers 0 composante par composante, donc sa norme tend vers 0
-    # On met à jour πk avec vk
-    πk .= πk .+ tk .* vk
-    
-    # On met à jour le poids des arêtes
-    for i=1:n
-      current_node = graph_copy.nodes[i]
-      for e in graph_copy.edges
-        if (e.start_node == current_node || e.end_node == current_node)
-          e.weight += πk[i]
-        end
-      end
-    end
-    
-    # On cherche le 1-arbre minimal correspondant au graphe mis à jour
-    Tk = find_minimum_1tree(graph_copy)
-    k += 1
-    tk = 1/(k+1)
-
-    dk = []
-    V = []
-    for node in Tk.nodes
-      voisins = neighbours(Tk, node)
-      push!(V, voisins)
-      push!(dk, length(voisins))
-    end
-  
-    # Calcul de vk pour le graphe mis à jour :
-    vk = dk .- 2
-    nvk = norm(vk)    
-
-    if verbose > 0 && mod(k, verbose) == 0
-      @info infoline
-      infoline = @sprintf "%5d  %9.2e  %7.1e" k tk nvk
-    end
-
-    if k == maxIter
-      println("maximum iteration criterion reached at k = $k")
-    elseif nvk ≤ ϵ 
-      println("algorithm converged to a optimal tour at k = $k")
-    end
-  end
-  Tk.name = "Optimal tour"
-  nvk_final = nvk
-  @info infoline
-  infoline = @sprintf "%5d  %9.2e  %7.1e" k tk nvk_final
-  return Tk
-end
-
-
-r = HK(cours, maxIter = 100, verbose=2) # converge en 15 itérations pour le noeud 1
-
-graph_tsp = build_graph("Phase 1/instances/stsp/bays29.tsp", "Graph_Test")
-r2 = HK(graph_tsp, verbose=10000, maxIter=200_000) # ne converge pas
-
-# Utilisation de la fonction
+## Phase 4
+photo = build_graph("Phase 4/shredder-julia/tsp/instances/alaska-railroad.tsp", "Graph_Test")
